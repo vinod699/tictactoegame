6c73d991fedc369387c3833eda4f83f589603ed8
vinod699 <kosarajuvinod9999@gmail.com>
2021-04-01 08:11:16 +0530

    uc3tictactoegame

--- src/tictactoegame/TicTacToeGame.java
+++ src/tictactoegame/TicTacToeGame.java
@@ -16,6 +16,9 @@ public class tictactoe {
 		
 		displayBoard(Board);
 		allowPlayerToChooseEitherXorO();
+		showBoard(Board);
+		
+		
 		
 		}
 
@@ -45,6 +48,14 @@ public class tictactoe {
 		else
 			computer = 'X';
 		
+	}
+	public static void showBoard(char[][] Board) {
+		System.out.println("--------Showing Current Board------");
+		System.out.println(Board[0][0]+ "|" + Board[0][1]+ "|" +Board[0][2]);
+		System.out.println("-+-+-");
+		System.out.println(Board[1][0]+ "|" + Board[1][1]+ "|" +Board[1][2]);
+		System.out.println("-+-+-");
+		System.out.println(Board[2][0]+ "|" + Board[2][1]+ "|" +Board[2][2]);
 	}
 	
 }
